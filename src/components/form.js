dbfd69d80d8dea0e5e19696926538e2b7949d88f
frederic zai <frederic.zai@gmail.com>
2024-05-12 15:42:18 +0200

    create search forms

---
+++ src/components/form.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+function SearchForm() {
+    // State 
+
+    // Effects
+    useEffect(() => {
+    }, []);
+
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default SearchForm;
