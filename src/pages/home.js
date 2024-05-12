3babac63700d4e8f5ea92110e241744c02117fad
frederic zai <frederic.zai@gmail.com>
2024-05-12 12:12:06 +0200

    create api connexion

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,27 @@
-
-import React from "react"
+import React, { useEffect } from "react"; 
+import axios from 'axios';
 
 function Home() {
 
+    // Effects
+    useEffect(() => {
+        const fetchData = async () => { 
+            try {
+                const response = await axios.get(`https://pokeapi.co/`); 
+                console.log(response.data); // Vous pouvez gérer la réponse ici
+            } catch(error) {
+                console.error('Error fetching data:', error); // Gestion des erreurs
+            }
+        };
 
+        fetchData(); // Appel de la fonction fetchData
+    }, []); // Le tableau vide signifie que ce code ne s'exécute qu'une seule fois lors du montage du composant
 
     return (
         <div>
             <h1>bonjour</h1>
         </div>
-    )
+    );
 }
 
-export default Home 
+export default Home;
