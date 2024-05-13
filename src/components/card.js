550f21ca17f122139713980d0e09ba29fc183de9
baba240491 <112175704+baba240491@users.noreply.github.com>
2024-05-13 15:30:07 +0200

    ajouter dans urll un limite de pokemon

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"; 
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import { Link } from "react-router-dom";
 
@@ -9,13 +9,13 @@ function CardItems() {
 
     // Effects
     useEffect(() => {
-        const fetchData = async () => { 
+        const fetchData = async () => {
             try {
-                const firstRes = await axios.get(`https://pokeapi.co/api/v2/pokemon/`);
+                const firstRes = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`);
                 const pokemonStartData = firstRes.data.results;
 
                 // Pour chaque Pokémon récuprer les détails
-                const pokemonDetail = await Promise.all(pokemonStartData.map(async (pokemon) =>{
+                const pokemonDetail = await Promise.all(pokemonStartData.map(async (pokemon) => {
                     const pokemonResponse = await axios.get(pokemon.url);
                     return pokemonResponse.data
                 }))
@@ -23,35 +23,35 @@ function CardItems() {
 
                 setPokemonRes(pokemonDetail);
                 setLoading(false);
-            } catch(error) {
-                console.error('Erreur:', error); 
+            } catch (error) {
+                console.error('Erreur:', error);
             }
         };
 
-        fetchData(); 
+        fetchData();
     }, []);
 
     return (
         <div>
             {
-                loading ? ( <p>Page Loading...</p>) : 
+                loading ? (<p>Page Loading...</p>) :
 
-                (pokemonRes && pokemonRes.map((post, id) => (
-                <Link to={`/pokemon/${post.id}`}>
-                    <div className="card" key={id}>
-                        <h2>Name : {post.name}</h2>
-                        <p>Number : {post.id}</p>
-                        <img src={post.sprites.front_default} alt="pokemon image" />
-                        <p>Types : {post.types.map((type) => type.type.name).join(', ')}</p>
-                        <p>Stats : </p>
-                        <ul>
-                            {post.stats.map((stat) => (
-                                <li key={stat.stat.name}>{stat.stat.name}:{stat.base_stat}</li>
-                            ))}
-                        </ul>
-                    </div>
-                </Link>
-                )))
+                    (pokemonRes && pokemonRes.map((post, id) => (
+                        <Link to={`/pokemon/${post.id}`}>
+                            <div className="card" key={id}>
+                                <h2>Name : {post.name}</h2>
+                                <p>Number : {post.id}</p>
+                                <img src={post.sprites.front_default} alt="pokemon image" />
+                                <p>Types : {post.types.map((type) => type.type.name).join(', ')}</p>
+                                <p>Stats : </p>
+                                <ul>
+                                    {post.stats.map((stat) => (
+                                        <li key={stat.stat.name}>{stat.stat.name}:{stat.base_stat}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </Link>
+                    )))
             }
         </div>
     );
